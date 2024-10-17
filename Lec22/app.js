87b162b4e49cb573b06956366191440c8ba7920d
Gaurav kumar <gauravkumar781902@gmail.com>
2024-10-17 09:28:32 +0530

    closure ,map &filter

---
+++ Lec22/app.js
@@ -0,0 +1,34 @@
+// function fun1(){
+// let a=10;
+// let b=20;
+// function fun2() {
+//     return a+b;
+// }
+// return fun2;
+// }
+// fun1();
+
+// let person={
+//     name:"Gaurav",
+//     age:"20"
+// }
+// for (let key in person) {
+//     console.log(key);
+    
+// }
+
+let arr=[1,2,3,4];
+// function fun1(item){
+//     return item*3;
+
+// }
+// let newarr=arr.map(fun1);
+// let newarr=arr.map(item=>item*4); //ES6
+// console.log(newarr);
+
+let newarr=arr.filter(item=>item%2);//oddelement
+
+
+console.log(newarr);
+
+
