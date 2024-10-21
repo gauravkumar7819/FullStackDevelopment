093fac491a199a78684e40f08d89f86878ee8fe2
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-10-21 10:34:29 +0530

    Synchronous

---
+++ Lec23/app.js
@@ -0,0 +1,17 @@
+console.log("hello");
+function waitAsec() {
+
+    let currtime=new Date().getTime();
+    let fut_time=currtime+6000; 
+    while (new Date().getTime()<fut_time);
+        
+    
+    
+}
+// let currtime=new Date().getTime();
+// let fut_time=currtime+1000;
+
+console.log(new Date().getTime());
+waitAsec();
+console.log("world");
+console.log("jhgg");
