678d51a699cc4f26118fba204a1823d2a0064519
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-10-23 08:24:33 +0530

    some changes

--- Lec23/app.js
+++ Lec23/app.js
@@ -1,17 +1,33 @@
+// console.log("hello");
+// function waitAsec() {
+//     let currtime=new Date().getTime();
+//     let fut_time=currtime+6000; 
+//     while (new Date().getTime()<fut_time);   }
+// // let currtime=new Date().getTime();
+// // let fut_time=currtime+1000;
+// console.log(new Date().getTime());
+// waitAsec();
+// console.log("world");
+// console.log("jhgg");
+console.log("log");
+// setTimeout(()=>{
+//     console.log("inside settimeout");   
+// },5000)
+// let id =setInterval(()=>{
+//     console.log("data is fechted");  
+// },0)
+// setTimeout(()=>{
+//    clearInterval(id)   
+// },5000)
 console.log("hello");
-function waitAsec() {
-
-    let currtime=new Date().getTime();
-    let fut_time=currtime+6000; 
-    while (new Date().getTime()<fut_time);
-        
-    
-    
-}
-// let currtime=new Date().getTime();
-// let fut_time=currtime+1000;
-
-console.log(new Date().getTime());
-waitAsec();
-console.log("world");
-console.log("jhgg");
+//  IIFE
+// (function IIFE() {
+//     console.log("IIFE");
+// })()
+( (a,b)=> {
+    console.log("IIFE 3");
+    console.log(`sum of a+b ${a+b}`); 
+})(2,4)
+//  CallBack
+//callback hell
+//pramid hell
