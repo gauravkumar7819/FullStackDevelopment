6aae01c3ae45138dd448d9f8667dfbe5f667f45e
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-10-23 08:42:51 +0530

    Promise

--- Lec24/app.js
+++ Lec24/app.js
@@ -1,15 +1,36 @@
 // Promise
-let p=new Promise((resolve,reject)=>{
-    let a=20;
-    let b=30;
-    if((a+b)>55){
-        return resolve(a+b);
-    }
-    else{
-        return reject("sum of a & b is less than 55")
-    }
-})
-p.then((res)=>{
+// let p=new Promise((resolve,reject)=>{
+//     let a=20;
+//     let b=30;
+//     if((a+b)>55){
+//         return resolve(a+b);
+//     }
+//     else{
+//         return reject("sum of a & b is less than 55")
+//     }
+// })
+// p.then((res)=>{
+//     console.log(res);
+    
+
+// }).catch((err)=>{
+//     console.log(err);
+    
+
+// })
+let sum=(a,b)=>{
+    return new Promise((resolve,reject)=>{
+        // let a=20;
+        // let b=30;
+        if((a+b)>5){
+            return resolve(a+b);
+        }
+        else{
+            return reject("sum of a & b is less than 55")
+        }
+    })
+}
+sum(56,56).then((res)=>{
     console.log(res);
     
 
