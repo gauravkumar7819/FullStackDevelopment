ab9e24e4cb0ee59e74ac190542485225d0ac4795
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-11-05 08:50:35 +0530

    DOM

--- Revision/script.js
+++ Revision/script.js
@@ -1,10 +1,17 @@
-document.getElementById('demo').innerHTML='hello'
-let a=document.getElementById('demo')
-console.log(a);
-function validateForm() {
-    let x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
+function random() {
+    let a=Math.floor((Math.random()*255)+1);
+let b=Math.floor((Math.random()*255)+1);
+let c=Math.floor((Math.random()*255)+1);
+
+return `rgb(${a},${b},${c})`;
+    
+}
+let btn=document.querySelector(".btn");
+// let main=document.querySelector(".container");
+btn.addEventListener('click',()=>{
+    let a=Math.floor((Math.random()*255)+1);
+    let b=Math.floor((Math.random()*255)+1);
+    let c=Math.floor((Math.random()*255)+1);
+    
+     document.querySelector(".container").style.backgroundColor= `rgb(${a},${b},${c})`;
+});
