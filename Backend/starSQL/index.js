8e8a1037f170ebd8cea0d70623ab5499cb51f059
Gaurav Kumar <gaurav.kumar_cs22@gla.ac.in>
2024-11-18 11:17:33 +0530

    REACT

--- Backend/starSQL/index.js
+++ Backend/starSQL/index.js
@@ -1,14 +1,41 @@
-const { faker } = require('@faker-js/faker');
+const { faker, ar } = require('@faker-js/faker');
 const mysql = require('mysql2');
+let createRandomUserfunction =()=> {
+    return [
+      userId=faker.string.uuid(),
+      username=faker.internet.username(), // before version 9.1.0, use userName()
+    //   email: faker.internet.email(),
+    //   avatar: faker.image.avatar(),
+      password= faker.internet.password(),
+    //   birthdate: faker.date.birthdate(),
+    //   registeredAt: faker.date.past(),
+    ];
+  }
+  let arr=[];
+
+  for (let i = 0; i <100; i++) {
+   arr.push(createRandomUserfunction());
+    
+  }
+//   for (const i of arr) {
+//     console.log(i);
+    
+//   }
+console.log(arr);
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'hari',
     password:"gaurav@82"
   });
+
+  let qu="insert into krishna (id ,name,password) values ?";
+  let user=[[23,"hari","@123"],[24,"hari","@123"],[26,"hari","@123"]
+];
   try{
   connection.query(
-    'select * from shyam', (err, results) =>{
+  qu,[arr], (err, results) =>{
         if(err){ throw err;}
       console.log(results);
     }
@@ -18,15 +45,8 @@ const connection = mysql.createConnection({
     
   }
   connection.end();
-// let createRandomUserfunction =()=> {
-//   return {
-//     userId: faker.string.uuid(),
-//     username: faker.internet.username(), // before version 9.1.0, use userName()
-//     email: faker.internet.email(),
-//     avatar: faker.image.avatar(),
-//     password: faker.internet.password(),
-//     birthdate: faker.date.birthdate(),
-//     registeredAt: faker.date.past(),
-//   };
-// }
-// console.log(createRandomUserfunction());
+
+
+
+
+  
--- Backend/starSQL/schema.sql
+++ Backend/starSQL/schema.sql
@@ -1,2 +1,5 @@
 -- create table name(name varchar(20),id int);
 show tables;
+CREATE table govind(id varchar(30),name  varchar(20),password varchar(20));
+CREATE table krishna(id varchar(100),name  varchar(100),password varchar(100));
+SELECT * from krishna;
